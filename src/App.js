09a90a3fc8435e76447086a9dc9421d10811750b
sebazdev <szuviria@gmail.com>
2022-09-14 22:34:05 -0300

    Clase de Componentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-import AvatarJsx from './components/AvatarJsx';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+// import AvatarJsx from './components/AvatarJsx';
+// import Button from './components/Button/Button'
+// import Counter from './components/Counter/Counter';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
+
+  // const accept = () => {
+  //   console.log('cancele')
+  // }
+
   return (
     <div id="1" className="App">
       {/* <header className="App-header "> */}
-        <img src={logo} className="App-logo" alt="logo" />
-        <AvatarJsx />
-        <p className="Titulo" style={{ backgroundColor: 'blue'}} onClick={() => console.log('click')}>BIENVENIDOS</p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* {React.createElement(AvatarJsx, { name: 'Jose', id: '2'})} */}
+        {/* <AvatarJsx name='Seba' id='1' desc='Tengo 28 años'/>
+        <Button label={'cancelar'} background='red' action={accept}/>
+        <Button label={'aceptar'} background={'green'} action={() => console.log('acepte')}/> */}
+        {/* <p className="Titulo" style={{ backgroundColor: 'blue'}} onClick={() => console.log('click')}>BIENVENIDOS</p>
         <button>boton</button>
-        <input />
+        <input /> */}
       {/* </header> */}
+        {/* <Counter /> */}
+        <Navbar />
+        <ItemListContainer greeting={'Estos son nuestros productos'}/>
     </div>
   );
 }
--- src/components/AvatarJsx.js
+++ src/components/AvatarJsx.js
@@ -1,10 +1,13 @@
-const AvatarJsx = (params) => {
-    const src = `https://randomuser.me/api/portraits/lego/1.jpg`
+const AvatarJsx = ({ name, id, desc }) => {
+    // console.log(props)
+    // const { name: nombre, id, desc } = props
+    const src = `https://randomuser.me/api/portraits/lego/${id}.jpg`
 
     return (
         <picture>
-            <img src={src} alt='seba'/>
-            Seba
+            <img src={src} alt={name}/>
+            {name}
+            {desc}
         </picture>
     )
 }
