c502383796a53cf56bba0802444b75c9784e6d88
sebazdev <szuviria@gmail.com>
2022-10-10 22:41:57 -0300

    Clase de rendering

--- src/App.js
+++ src/App.js
@@ -7,26 +7,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartContextProvider } from './context/CartContext'
 // export const Context = createContext()
 // export const CartContext = createContext()
+import { NotificationProvider } from './notification/Notification';
 
 function App() {
  
   return (
     <div className="App"> 
-      {/* <Context.Provider value={{ value1: 'valor a compoartir', value2: 'otro valor a compartir'}}> */}
-        {/* <CartContext.Provider value={{ addItem, removeItem }}> */}
-          <CartContextProvider>
-            <BrowserRouter>
-              <Navbar />
-              <Routes>
-                <Route path='/' element={<ItemListContainer greeting='Listado de todos los productos'/>}/>
-                <Route path='/category/:categoryId' element={<ItemListContainer greeting='Listado filtrado'/>} />
-                <Route path='/detail/:productId' element={<ItemDetailContainer />} />  
-                <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
-              </Routes>
-            </BrowserRouter>
-          </CartContextProvider>
-        {/* </CartContext.Provider> */}
-      {/* </Context.Provider> */}
+          <NotificationProvider>
+            <CartContextProvider>
+              <BrowserRouter>
+                <Navbar />
+                <Routes>
+                  <Route path='/' element={<ItemListContainer greeting='Listado de todos los productos'/>}/>
+                  <Route path='/category/:categoryId' element={<ItemListContainer greeting='Listado filtrado'/>} />
+                  <Route path='/detail/:productId' element={<ItemDetailContainer />} />  
+                  <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
+                </Routes>
+              </BrowserRouter>
+            </CartContextProvider>
+          </NotificationProvider>
     </div>
   );
 }
